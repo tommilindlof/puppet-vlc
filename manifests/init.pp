703b328097feccf17dab785a1a25b55cae2441db
Tommi Lindlof <tommi.lindlof@myy.haaga-helia.fi>
2016-11-16 11:23:57 +0200

    Added Firefox to the installation.

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,3 +10,48 @@ class vlc(
 
   include vlc::package, vlc::config
 }
+
+define firefox(
+	$version='firefox-mozilla-build',
+	$removeiceweasel=true,
+	$ensure='latest'
+) {
+	case $::operatingsystem {
+		'ubuntu', 'debian': {
+			apt::source {'firefoxsource':
+			  name => 'downloads-sourceforge-net_project_ubuntuzilla_mozilla_apt',
+				location    => 'http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt',
+				release     => ' ',
+				repos       => 'all main',
+				key        => 'C1289A29',
+			key_server => 'keyserver.ubuntu.com',
+				include_src => false
+			}
+
+		}
+		default: {
+			apt::source {'firefoxsource':
+        name => 'downloads-sourceforge-net_project_ubuntuzilla_mozilla_apt',
+        location    => 'http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt',
+        release     => ' ',
+        repos       => 'all main',
+        key        => 'C1289A29',
+        key_server => 'keyserver.ubuntu.com',
+        include_src => false
+      }
+		}
+	}
+	
+	if $removeiceweasel == true {
+		package{"removeiceweasel":
+			name=>"iceweasel",
+			ensure=>"purged"
+		}
+	}
+	
+	package{"installfirefox":
+		name=>$version,
+		ensure=>$ensure,
+		require=>Apt::Source['firefoxsource']
+	}
+}
